62cc37fe027508cda02e88c56cd04d39ec27de9c
Dave Taht <dave@taht.net>
2017-03-13 12:03:38 -0700

    some more notes revision of the command set etc

--- clients/tabalb/commands.c
+++ clients/tabalb/commands.c
@@ -66,6 +66,7 @@ my callbacks = {
 
 int dump_routers(router_filt filt) {
 	int c = 0;
+	push_regs();
 	for (int i = 0; i < routers.size; i++) {
 		if(routers[i].flags & filt) {
 		c++;
@@ -73,6 +74,7 @@ int dump_routers(router_filt filt) {
 			format_eui64(routers.a[i]));
 		}
 	}
+	pop_regs();
 	return c;
 }
 
--- clients/tabalb/simd.h
+++ clients/tabalb/simd.h
@@ -8,17 +8,38 @@
 #ifndef SIMD_H
 #define SIMD_H
 
-// This ultimately will try to hammer the neon and sse abstractions into submission
+// This ultimately will try to hammer the neon and sse abstractions and API into submission
+
+// FIXME - I need to wedge 32 bytes into a register correctly. Somehow
 
 #ifdef HAVE_NEON
 #include <arm_neon.h>
+static inline void push_regs() { }
+static inline void pop_regs() { }
+typedef uint32x4_t usimd __attribute__ ((vector_size (16)));
 #endif
 
 #ifdef HAVE_SSE4
+#include <xmmintrin.h>
 #include <emmintrin.h>
+static inline void push_regs() { }
+static inline void pop_regs() { }
+typedef unsigned int usimd __attribute__ ((vector_size (16)));
 #endif
 
+#ifdef HAVE_ADAPTEVA
+#include <xmmintrin.h>
+#include <emmintrin.h>
+static inline void push_regs() { }
+static inline void pop_regs() { }
 typedef unsigned int usimd __attribute__ ((vector_size (16)));
+#endif
+
+#if !(defined (HAVE_NEON) | defined(HAVE_SSE4) | defined(HAVE_ADAPTEVA))
+static inline void push_regs() { }
+static inline void pop_regs() { }
+typedef unsigned int usimd __attribute__ ((vector_size (16)));
+#endif
 
 typedef union Vec4 {
     usimd p;
@@ -28,6 +49,5 @@ typedef union Vec4 {
     uint16_t s[8];
     uint8_t c[16];
 } Vec4_t;
-
 
 #endif
