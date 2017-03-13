a86a74803414351747a7927796ee4abf1bfdb27a
Dave Taht <dave@taht.net>
2017-03-13 10:56:51 -0700

    Basic simd stuff
    Boy is this hard to deal with portably.

--- clients/tabalb/simd.h
+++ clients/tabalb/simd.h
@@ -8,6 +8,26 @@
 #ifndef SIMD_H
 #define SIMD_H
 
-// This ultimately will try to hammer the neon and sse abstractions into bay
+// This ultimately will try to hammer the neon and sse abstractions into submission
+
+#ifdef HAVE_NEON
+#include <arm_neon.h>
+#endif
+
+#ifdef HAVE_SSE4
+#include <emmintrin.h>
+#endif
+
+typedef unsigned int usimd __attribute__ ((vector_size (16)));
+
+typedef union Vec4 {
+    usimd p;
+    float e[4];
+    int32_t i[4];
+    uint32_t u[4];
+    uint16_t s[8];
+    uint8_t c[16];
+} Vec4_t;
+
 
 #endif
