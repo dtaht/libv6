b5719d51a54a7fe4f5e0a52ced6cce5e1c3a3e19
Dave Taht <dave@taht.net>
2017-03-13 01:04:51 -0700

    I am too lazy to figure out all the dependencies as yet.

---
+++ clients/tabalb/shared.h
@@ -0,0 +1,40 @@
+/**
+ * shared.h
+ * I am sufficiently lazy to not worry about this at the moment
+ * Dave Taht
+ * 2017-03-12
+ */
+
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <net/route.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+#include <asm/types.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+#include <linux/if_bridge.h>
+#include <linux/filter.h>
+#include <linux/bpf.h>
+#include <linux/fib_rules.h>
+#include <net/if_arp.h>
+
+#include <netinet/in.h>
+
+#endif
