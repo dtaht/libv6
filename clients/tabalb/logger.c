c3762011d9de450f3085146ceb4fbad74a84009a
Dave Taht <dave@taht.net>
2017-03-13 10:55:15 -0700

    Some updates to how the logger concept will work
    I was unaware that C99 standardized __func__ until now.
    Joy. Bliss. Rapture.
    
    I want to be able to both selectively compile out debugging messages
    and find these eventually into a ring buffer that can overflow.

--- clients/tabalb/logger.c
+++ clients/tabalb/logger.c
@@ -12,25 +12,10 @@
 
 #include "logger.h"
 
-//typedef uint32x4_t usimd;
-
-typedef unsigned int usimd __attribute__ ((vector_size (16)));
-
 int v4mapped_vec(usimd a) { return 0; }
 int v4mapped_str(char * a) { return 0; }
 
-const char * format_prefix_vec(const usimd prefix, unsigned char plen);
-const char * format_prefix_str(const unsigned char *prefix, unsigned char plen);
-
-typedef union Vec4 {
-    usimd p;
-    float e[4];
-    int i[4];
-    unsigned int u[4];
-    unsigned char c[16];
-} Vec4_t;
-
-#define format_prefix(X,v) _Generic((X),  \
+#define format_prefix_t(X,v) _Generic((X),  \
         char * : format_prefix_str,       \
         usimd  : format_prefix_vec,       \
         default: format_prefix_vec,	  \
@@ -43,81 +28,6 @@ typedef union Vec4 {
                                 usimd  : v4mapped_vec,    \
         default: v4mapped_vec			      \
 		)(X)
-
-const char *
-format_prefix_vec(const usimd prefix, unsigned char plen)
-{
-    static __thread char buf[4][INET6_ADDRSTRLEN + 4] = {0};
-    static __thread int i = 0;
-    int n;
-    Vec4_t p2;
-    p2.p = prefix;
-    i = (i + 1) % 4; // WTF in a 64 bit arch? Let you call this 4 times?
-    if(plen >= 96 && v4mapped(prefix)) {
-            inet_ntop(AF_INET, &p2.c[12], buf[i], INET6_ADDRSTRLEN); // ??
-        n = strlen(buf[i]);
-        snprintf(buf[i] + n, INET6_ADDRSTRLEN + 4 - n, "/%d", plen - 96);
-    } else {
-      inet_ntop(AF_INET6, &p2.c, buf[i], INET6_ADDRSTRLEN);
-        n = strlen(buf[i]);
-        snprintf(buf[i] + n, INET6_ADDRSTRLEN + 4 - n, "/%d", plen);
-    }
-    return buf[i];
-}
-
-const char *
-format_address(const unsigned char *address)
-{
-    static char buf[4][INET6_ADDRSTRLEN];
-    static int i = 0;
-    i = (i + 1) % 4;
-    if(v4mapped(address))
-        inet_ntop(AF_INET, address + 12, buf[i], INET6_ADDRSTRLEN);
-    else
-        inet_ntop(AF_INET6, address, buf[i], INET6_ADDRSTRLEN);
-    return buf[i];
-}
-
-const char *
-format_prefix_str(const unsigned char *prefix, unsigned char plen)
-{
-    static char buf[4][INET6_ADDRSTRLEN + 4];
-    static int i = 0;
-    int n;
-    i = (i + 1) % 4;
-    if(plen >= 96 && v4mapped(prefix)) {
-        inet_ntop(AF_INET, prefix + 12, buf[i], INET6_ADDRSTRLEN);
-        n = strlen(buf[i]);
-        snprintf(buf[i] + n, INET6_ADDRSTRLEN + 4 - n, "/%d", plen - 96);
-    } else {
-        inet_ntop(AF_INET6, prefix, buf[i], INET6_ADDRSTRLEN);
-        n = strlen(buf[i]);
-        snprintf(buf[i] + n, INET6_ADDRSTRLEN + 4 - n, "/%d", plen);
-    }
-    return buf[i];
-}
-
-const char *
-format_eui64(const unsigned char *eui)
-{
-    static char buf[4][28];
-    static int i = 0;
-    i = (i + 1) % 4;
-    snprintf(buf[i], 28, "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
-             eui[0], eui[1], eui[2], eui[3],
-             eui[4], eui[5], eui[6], eui[7]);
-    return buf[i];
-}
-
-const char *
-format_thousands(unsigned int value)
-{
-    static char buf[4][15];
-    static int i = 0;
-    i = (i + 1) % 4;
-    snprintf(buf[i], 15, "%d.%.3d", value / 1000, value % 1000);
-    return buf[i];
-}
 
 #ifdef TEST
 #define TPREFIX1 "fd80::1"
--- clients/tabalb/logger.h
+++ clients/tabalb/logger.h
@@ -1,1 +1,25 @@
-/* Logger export */
+/**
+ * logger.h
+ * Dave Taht
+ * 2017-03-13
+ */
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#define logger(a,b,...) log(a,b,__LINE__,__func__, ...)
+
+extern int print(...);
+extern int log(int errlevel, int err, int line, char *file, char *func, ...);
+
+#define DONOTHING do { } while 0
+
+// Logging is done both on severity and on type or file basis
+
+#ifdef DEBUG
+#define debug_log(a, b, ...) a < DEBUG && b & ~DEBUG ? DONOTHING : logger(errlevel, err, ... )
+#else
+#define debug_log(a, b, ...) DONOTHING
+#endif
+
+#endif
