2bb6763ef1adc852fb229478e7ce0117f0c4c0cd
Dave Taht <dave@taht.net>
2017-03-15 16:17:47 -0700

    introduce stagger.h header
    I've written this so many different ways as a primitive so many times
    it's hard to figure out the best way to do it again.
    
    I should really code up the c version as a command line tool again.
    it was really handy in the late 90s where I'd do a
    
    pdsh -g all_servers stagger restart_server
    
    The concept here is different, in that I have one or more threads
    running (in a conventional arch), or a really fast way to multicast
    to a ton of processors (parallella or fpga).

---
+++ clients/tabalb/stagger.h
@@ -0,0 +1,26 @@
+/**
+ * stagger.h
+ *
+ * Dave Taht
+ * 2017-03-15
+ */
+
+#ifndef STAGGER_H
+#define STAGGER_H
+
+/* stagger(mask, interval, code) will mean:
+   "send a message to mask to run this block of code,
+    each starting randomly within this interval"
+
+The message will go out to all listeners, which will then randomly
+take that interval and go to sleep, then run.
+
+It's intended to be a generic replacement for "for_each_interface",
+for_each_neighbor, etc.
+
+for now, punt, because I have to think about it.
+
+*/
+
+
+#endif
