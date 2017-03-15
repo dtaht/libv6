e57d203f96966a7171d2ab765de151fbc16fb7ed
Dave Taht <dave@taht.net>
2017-03-15 16:24:06 -0700

    Fix makefile again

--- clients/tabalb/includes/align.h
+++ clients/tabalb/includes/align.h
@@ -9,5 +9,8 @@
 #define ALIGN_H
 
 // Handle alignment and probably other special memory handling
+// ALIGN_CACHELINE?
+// get_aligned_memory is now a c11 primitive
+// get_special_memory (core?)?
 
 #endif
