c6082e65319fb81def9ea98dc63d562c30bf8199
Dave Taht <dave@taht.net>
2017-03-16 20:23:44 -0700

    Thoughts about how to hook printf
    Actually I'd like a printf entirely lacking in fp hooks...
    not sure how to get there.

---
+++ clients/tabalb/printf_hooks.c
@@ -0,0 +1,24 @@
+/**
+ * printf_hooks.c
+ *
+ * Dave Taht
+ * 2017-03-16
+ */
+
+#include "printf_hooks.h"
+
+// We patch printf, sprintf to do
+
+// MAC : %M
+
+// EUI 64: %E
+
+// V4PREFIX: %P
+
+// V6PREFIX: %O
+
+// And we flip the endian perhaps this way also
+
+// pass pointers to regs? what?
+
+
