ecd3313a8e31d812b4f5479cdc5bab7ab3ef7da0
Dave Taht <dave@taht.net>
2017-03-18 12:19:15 -0700

    I really want to announce I have more capabilities

---
+++ clients/tabalb/includes/message_capabilities.h
@@ -0,0 +1,20 @@
+/**
+ * message_capabilities.h
+ *
+ * Dave Taht
+ * 2017-03-18
+ */
+
+#ifndef MESSAGE_CAPABILITIES_H
+#define MESSAGE_CAPABILITIES_H
+
+typedef struct {
+	uint16_t gps:1;
+	uint16_t routes:1;
+	uint16_t routers:1;
+	uint16_t interfaces:1;
+	uint16_t uptime:1;
+	uint16_t load:1;
+	// ?
+}
+#endif
