1f9cded6984d55296b9eed402ab2f60bdc364945
Dave Taht <dave@taht.net>
2017-03-18 12:19:58 -0700

    More capabilities

---
+++ clients/tabalb/includes/cpu_features.h
@@ -0,0 +1,15 @@
+/**
+ * cpu_features.h
+ *
+ * Dave Taht
+ * 2017-03-17
+ */
+
+#ifndef CPU_FEATURES_H
+#define CPU_FEATURES_H
+// https://en.wikipedia.org/wiki/Bit_Manipulation_Instruction_Sets
+// Intel sets the BMI bit in CPUID
+// LZCNT is advertised via the ABM cpuid
+// BLSMASK looks useful
+
+#endif
---
+++ clients/tabalb/includes/message_extensions.h
@@ -0,0 +1,33 @@
+/**
+ * message_extensions.h
+ *
+ * Dave Taht
+ * 2017-03-17
+ */
+
+#ifndef MESSAGE_EXTENSIONS_H
+#define MESSAGE_EXTENSIONS_H
+
+#define ASK_STATS
+#define SEND_STATS
+#define AE2
+#define AE2
+#define FEC
+#aggregation
+#define CAPABILITIES
+uptime
+cpu
+arch
+rttprobe
+location
+name
+nat
+
+connections
+network diameter discovery - use the hopcount
+slowdown
+CRC
+encrypted or not - flowflags
+sendmmsg
+colliding router id
+#endif
