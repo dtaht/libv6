3fbc56ea28d754fcfec1b922b9651cc2269d3a9b
Dave Taht <dave@taht.net>
2017-03-20 11:45:01 -0700

    A saner place to stick my experiments
    Needed struct passing to work sanely.

---
+++ erm/src/exper/structpassing.c
@@ -0,0 +1,64 @@
+// Test structure assignment code generation capabilities
+
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+	uint32_t a;
+} global_flags_t;
+
+global_flags_t tflags;
+
+#include "preprocessor.h"
+#include "simd.h"
+
+typedef struct {
+	uint32_t flags;
+} flags_t;
+
+typedef struct {
+	uint8_t a VECTOR(16);
+} v6addr_t;
+
+typedef struct {
+	v6addr_t a;
+	flags_t f;
+} compound_struct_t;
+
+// These should do the right thing.
+
+extern compound_struct_t fool_compiler1(compound_struct_t a, compound_struct_t b);
+extern compound_struct_t fool_compiler2(flags_t af, v6addr_t av, 
+				        flags_t bf, v6addr_t bv);
+
+extern v6addr_t fool_compiler3(flags_t af, v6addr_t av, 
+			       flags_t bf, v6addr_t bv);
+
+extern flags_t fool_compiler4(flags_t af, v6addr_t av, 
+			      flags_t bf, v6addr_t bv);
+
+int check_flags(flags_t f) {
+	if(f.flags | 1) printf("Got 1\n");
+	if(f.flags | 1) printf("Got 1\n");
+	return f.flags;
+}
+
+#define TESTS1(n,a,b,result) result = fool_compiler##n(a,b); check_flags(r.f)
+#define TESTS2(n,g,g1,k,k1,result) result = fool_compiler##n(g.f,g1.a,k.f,k1.a); check_flags(r.f)
+
+int main() {
+	compound_struct_t c = {0};
+	compound_struct_t d = {0};
+	compound_struct_t r;
+	c.f.flags = 1;
+	d.f.flags = 2;
+	TESTS1(1,c,d,r); // does not generate SSE instructions
+	TESTS1(1,c,d,r); // does not generate SSE instructions
+	TESTS2(2,c,c,d,d,r); // does generate SSE instructions
+	TESTS2(2,c,c,d,d,r); // does generate SSE instructions
+	TESTS2(3,c,c,d,d,r.a); // does generate SSE instructions
+	TESTS2(3,c,c,d,d,r.a); // does generate SSE instructions
+	TESTS2(4,c,c,d,d,r.f); // does generate SSE instructions
+	TESTS2(4,c,c,d,d,r.f); // does generate SSE instructions
+	return 0;
+}
