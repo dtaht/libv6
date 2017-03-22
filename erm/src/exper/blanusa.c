e49b028aff9da30836838b9b6c0418823d377cf3
Dave Taht <dave@taht.net>
2017-03-21 23:37:50 -0700

    Integer safe version. But boy does that get big fast
    Blanusa 62181: 7.46642e+95
    Blanusa 62182: 7.46882e+95
    Blanusa 62183: 7.47122e+95
    Blanusa 62184: 7.47362e+95
    Blanusa 62185: 7.47603e+95
    Blanusa 62186: 7.47843e+95
    
    Blanusa 16: 7.22987e+23
    
    Not confident I got the equation typed in right,
    will check in the morning.
    
    If you are pushing towards a goal tho, you can try to get back to 3 or 1
    as your solution - and I'll have to plot this a couple ways.
    
    Blanusa 127: 1.13394e+42
    Blanusa 128: 1.32706e+42
    Blanusa 129: 1.55117e+42
    Blanusa 130: 1.81093e+42
    Blanusa 131: 2.11167e+42
    Blanusa 132: 2.45948e+42
    Blanusa 133: 2.86128e+42
    Blanusa 134: 3.32493e+42
    Blanusa 135: 3.85939e+42
    Blanusa 136: 4.47482e+42
    Blanusa 137: 5.18276e+42
    Blanusa 138: 5.99627e+42
    Blanusa 139: 6.93016e+42
    Blanusa 140: 8.00116e+42
    Blanusa 141: 9.22822e+42

--- erm/src/exper/blanusa.c
+++ erm/src/exper/blanusa.c
@@ -8,16 +8,22 @@
 #include <stdint.h>
 #include <math.h>
 
+#include "preprocessor.h"
+#include "debug.h"
+#include "erm_types.h"
+#include "simd.h"
+
+// Worse, we need saturating arith
+
 #define generic uint16_t
 #define blanusa blanusauint16_t
-
+#define mypow(a,b) ((a) << (b))
 #include "blanusa.h"
+
 #undef generic
 #undef blanusa
 #define generic uint32_t
 #define blanusa blanusauint32_t
-
-
 #include "blanusa.h"
 
 #undef generic
@@ -25,8 +31,31 @@
 #define generic uint64_t
 #define blanusa blanusauint64_t
 #include "blanusa.h"
+
 #undef generic
 #undef blanusa
+#define generic int16_t
+#define blanusa blanusaint16_t
+#include "blanusa.h"
+
+#undef generic
+#undef blanusa
+#define generic int32_t
+#define blanusa blanusaint32_t
+#include "blanusa.h"
+
+#undef generic
+#undef blanusa
+#define generic int64_t
+#define blanusa blanusaint64_t
+#include "blanusa.h"
+
+// Floating point is different. I can improve it but I don't care at the moment
+
+#undef generic
+#undef blanusa
+#undef mypow
+#define mypow pow
 #define generic float
 #define blanusa blanusafloat
 #include "blanusa.h"
@@ -38,4 +67,17 @@
 #undef generic
 #undef blanusa
 
+#ifdef DEBUG_MODULE
+#include <stdio.h>
+int main()
+{
+  double d;
+  for(int i = 0; i < 0xffff; i++) {
+    d = blanusadouble(i);
+    printf("Blanusa %d: %g\n", i, d);
+  }
+
+  return 0;
+}
+#endif
 
--- erm/src/exper/blanusa.h
+++ erm/src/exper/blanusa.h
@@ -7,12 +7,12 @@
  */
 
 // This is one of those functions that is so totally obviously unsafe
-// to have as a macro. I don't even trust pow in this case.
+// to have as a macro. I don't even trust mypow in this case.
 
 generic blanusa(generic x); // PURE;
 
 generic blanusa(generic x) {
-return pow((x-3)*(x-1),3) * ((x+1)*(x+2)) *
-	(pow(x,4) + pow(x,3) - 7 * pow(x,2) - 5 * x + 6) *
-	 pow(pow(x,4) + pow(x,3) - 5 * (pow(x,2) - 3 * x + 4),2);
+return mypow((x-3)*(x-1),3) * ((x+1)*(x+2)) *
+	(mypow(x,4) + mypow(x,3) - 7 * mypow(x,2) - 5 * x + 6) *
+	 mypow(mypow(x,4) + mypow(x,3) - 5 * (mypow(x,2) - 3 * x + 4),2);
 }
