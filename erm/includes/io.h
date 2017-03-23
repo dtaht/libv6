2d057ce2a8c16981a8489d5be8f06f70584523ec
Dave Taht <dave@taht.net>
2017-03-22 19:53:59 -0700

    io brought to bear with generics
    totally non working code but at least it compiles

--- erm/includes/io.h
+++ erm/includes/io.h
@@ -9,8 +9,10 @@
 #define IO_H
 #include "shared.h"
 
-extern const char* format_address(const ubase_t a);
-extern const char* format_prefix(const ubase_t a);
+extern char* format_address(const ubase_t a);
+extern const char* format_prefix_ref(const ubase_t a);
+extern const char* format_prefix_str(const char *a);
+extern const char* format_prefix_vec(v128 a);
 extern const char* format_eui64(const unsigned char* eui);
 extern const char* format_thousands(unsigned int value);
 
--- erm/src/io.c
+++ erm/src/io.c
@@ -5,12 +5,18 @@
  * 2017-03-12
  */
 
+#include "shared.h"
 #include "io.h"
 #include "debug.h"
 #include "init.h"
-#include "shared.h"
 
-#define is_v4mapped(u) (addrdata[u].v4)
+bool is_v4mapped(const ubase_t u) {
+return (addrdata[u].v4);
+}
+
+bool is_v4mapped_str(const char *u) {
+	return 1;
+}
 
 // I will probably regret this unless I make it atomic or per cpu or per
 // thread
@@ -21,19 +27,36 @@ static int neti = 0;
 
 // Why do we not have the plen check here?
 
-const char* format_address(const ubase_t a)
+const char *format_address_str(const char *a) {
+  char* buf = getnetbuf(NULL); // const is a lie
+  inet_ntop(is_v4mapped_str(a) ? AF_INET : AF_INET6,
+  a + 12 * is_v4mapped_str(a), buf, INET6_ADDRSTRLEN);
+  return buf;
+}
+
+char* format_address_ref(const ubase_t a)
 {
   char* address = (char*)&addresses[a];
-  char* buf = getnetbuf(NULL);
+  char* buf = getnetbuf(NULL); // const is a lie
 
   inet_ntop(is_v4mapped(a) ? AF_INET : AF_INET6,
             address + 12 * is_v4mapped(a), buf, INET6_ADDRSTRLEN);
   return buf;
 }
 
-const char* format_prefix(const ubase_t a)
+const char* format_prefix_str(const char *a)
 {
-  char* buf = format_address(a);
+  char* buf = format_address_str(a);
+  int plen = strlen(a);
+  /* might be pushing my luck here, using the buf twice */
+  snprintf(buf, strlen(buf) + 4, "%s/%d", buf,
+           (plen >= 96) * is_v4mapped_str(a) ? plen - 96 : plen);
+  return buf;
+}
+
+const char* format_prefix_ref(const ubase_t a)
+{
+  char* buf = format_address_ref(a);
   int plen = addrdata[a].len;
   /* might be pushing my luck here, using the buf twice */
   snprintf(buf, strlen(buf) + 4, "%s/%d", buf,
@@ -57,5 +80,11 @@ const char* format_thousands(unsigned int value)
 }
 
 #ifdef DEBUG_MODULE
-MFIXME
+addrflags_t* addrdata;
+ip_addr *addresses;
+int main() {
+	addresses = calloc(255,sizeof(ip_addr));
+	printf("do some io!\n");
+	return -1;
+}
 #endif
