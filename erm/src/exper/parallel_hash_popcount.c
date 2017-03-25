0c0369c5462c8595ebf47753561f979bb2790ecf
Dave Taht <dave@taht.net>
2017-03-24 23:38:51 -0700

    Well I should be able to do better than this
      40080a:       31 c9                   xor    %ecx,%ecx
      40080c:       48 8b 3d 85 08 20 00    mov    0x200885(%rip),%rdi        # 601098 <test4+0x18>
      400813:       48 8b 35 76 08 20 00    mov    0x200876(%rip),%rsi        # 601090 <test4+0x10>
      40081a:       48 8b 15 67 08 20 00    mov    0x200867(%rip),%rdx        # 601088 <test4+0x8>
      400821:       48 8b 05 58 08 20 00    mov    0x200858(%rip),%rax        # 601080 <test4>
      400828:       f3 48 0f b8 c0          popcnt %rax,%rax
      40082d:       48 01 c3                add    %rax,%rbx
      400830:       f3 48 0f b8 f6          popcnt %rsi,%rsi
      400835:       48 01 f1                add    %rsi,%rcx
      400838:       f3 48 0f b8 d2          popcnt %rdx,%rdx
      40083d:       48 01 d3                add    %rdx,%rbx
      400840:       f3 48 0f b8 ff          popcnt %rdi,%rdi
      400845:       48 01 f9                add    %rdi,%rcx
      400848:       be 24 0a 40 00          mov    $0x400a24,%esi
      40084d:       0f b6 d3                movzbl %bl,%edx
      400850:       0f b6 c9                movzbl %cl,%ecx
      400853:       bf 01 00 00 00          mov    $0x1,%edi
      400858:       31 c0                   xor    %eax,%eax
      40085a:       e8 f1 fc ff ff          callq  400550 <__printf_chk@plt>
    
    Admittedly most of the overhead is printf.....

--- erm/src/exper/parallel_hash_popcount.c
+++ erm/src/exper/parallel_hash_popcount.c
@@ -22,8 +22,8 @@
    Running a hot cache for their ins. As if our cpus did nothing more than hash
    stuff all day.
 
-   Not, as if, I run the hash often enough to care about that! I need a quick
-   hash on some fairly small fixed length values, then move on. Start time is
+   I don't run the hash often enough to care about that! I need a quick hash on
+   some fairly small fixed length values, then to move on. Start time is
    important. Now, it happens that cake ended up using a similar hash on 3
    values at the same time ... and I'm wondering that can yield to running 4
    hashes in parallel in the simd unit, which would be pretty awesome...
@@ -43,8 +43,102 @@ Well, first benchmarking what straightline code does makes the most sense. :)
 */
 
 
-#include "parallel_hash_popcount.h"
+#include <math.h>
+#include <stdint.h>
+#include <tgmath.h>
+
+#include "debug.h"
+#include "erm_types.h"
+#include "preprocessor.h"
+#include "simd.h"
+
+typedef struct {
+	u8 one;
+	u8 two;
+} twocount;
+
+inline twocount popcount2cheaper(const u64 *buf) {
+    u64 cnt[1] = {0};
+    const int i = 0;
+    __asm__ __volatile__(
+            "popcnt %2, %2  \n\t"
+            "add %2, %0     \n\t"
+            "popcnt %4, %4  \n\t"
+            "add %4, %1     \n\t"
+            "popcnt %3, %3  \n\t"
+            "add %3, %0     \n\t"
+            "popcnt %5, %5  \n\t"
+            "add %5, %1     \n\t"
+            : "+r" (cnt[0]), "+r" (cnt[1])
+            : "r"  (buf[i]), "r"  (buf[i+1]), "r"  (buf[i+2]), "r"  (buf[i+3])
+                );
+  twocount t;
+  t.one =   cnt[0];
+  t.two =   cnt[1];
+  return t;
+}
+
+
+inline twocount popcount2(const u64 *buf) {
+    u64 cnt[4] = {0};
+    const int i = 0;
+    __asm__ __volatile__(
+            "popcnt %4, %4  \n\t"
+            "add %4, %0     \n\t"
+            "popcnt %5, %5  \n\t"
+            "add %5, %1     \n\t"
+            "popcnt %6, %6  \n\t"
+            "add %6, %2     \n\t"
+            "popcnt %7, %7  \n\t"
+            "add %7, %3     \n\t"
+            : "+r" (cnt[0]), "+r" (cnt[1]), "+r" (cnt[2]), "+r" (cnt[3])
+            : "r"  (buf[i]), "r"  (buf[i+1]), "r"  (buf[i+2]), "r"  (buf[i+3])
+                );
+  twocount t;
+  t.one =   cnt[0] + cnt[1] ;
+  t.two =   cnt[2] + cnt[3] ;
+  return t;
+}
+
 
 #ifdef DEBUG_MODULE
-FIXME
+#define LOGGER_INFO(where, fmt, ...)
+
+#ifndef DONOTHING
+#define DONOTHING                                                            \
+  do {                                                                       \
+  } while(0)
+#endif
+
+#ifndef CALLOCA
+#define CALLOCA(type, dest, size, num)                                       \
+  type dest __attribute__((aligned(16)));                                    \
+  dest = calloc(size, num)
+#endif
+
+#include "align.h"
+#include "cycles_bench.h"
+#include "erm_logger.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+u64 test1[4] = {0};
+u64 test2[4] = {0xff, 0xff, 0xff, 0x7f};
+u64 test3[4] = {0xff7f,0x7fffffff, 0x7f, 0xff };
+u64 test4[4] = {0xff7f,0x7fffffff, 0xff, 0x7f };
+
+int main() {
+	twocount t = popcount2(&test1);
+	printf("popzero: %d %d\n",t.one, t.two);
+	t = popcount2(&test2);
+	printf("popzero: %d %d\n",t.one, t.two);
+	t = popcount2(&test3);
+	printf("popzero: %d %d\n",t.one, t.two);
+	t = popcount2cheaper(&test4);
+	printf("pop2cheaper: %d %d\n",t.one, t.two);
+	return 0;
+}
+
 #endif
