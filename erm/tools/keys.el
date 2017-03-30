e66d193437c6423299751fc5996a4dc691a34c14
dave taht <dave@taht.net>
2017-03-30 14:52:25 -0700

    Some basic symbology
    set theory and boolean operations sometimes intersect badly.
    
    And I never liked the 4 valued logic symbols either.
    
    Just wedging a few of these on the super key (in emacs), might help,
    but theres a large unicode subset or three to look through.
    
    Nonworking code of course, as usual. But certainly "code".

---
+++ erm/tools/keys.el
@@ -0,0 +1,28 @@
+;; keyboard shortcuts
+
+(define-key s-= ←) ; store
+
+s-+ ; NAR +
+s-- ; NAR -
+s-* ; NAR *
+s-_ ; message
+
+; some keystrokes I don't know what to do with yet
+
+s-@
+s-#
+s-$
+s-%
+
+;; Boolean operations?
+
+s-? ; ternary NAR
+s-& ⊖; boolean ANDNAR - no this is a set difference, use that? Or △
+s-^ ⊕; boolean NORNAR - set union
+s-! ; boolean NOTNAR
+
+;; Set operations
+
+s-u ⊎; multiset union ( basically a var-args function?) 
+
+https://en.wiktionary.org/wiki/symmetric_difference
