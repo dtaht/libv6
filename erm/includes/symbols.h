11b59423c9a308cd3e6bb4eb88f66edce6ebe6b2
dave taht <dave@taht.net>
2017-04-05 15:46:32 -0700

    Some symbols from boolean algebra and mathmatics
    not that I know where I am going with it. I like that not and
    has a specific symbol for it, that C doesn't have. It kind of
    exposes the infix limitations there.

---
+++ erm/includes/symbols.h
@@ -0,0 +1,22 @@
+//    Symbol C equivalent
+
+#define ¬    ~
+#define ∧    &
+#define ⊼    nand // ~(a & b) or !(a && b)
+#define ∨    |
+#define ⊽    nor
+#define ∥    ||    // logical or
+#define ⊕    ^  // xor but also the direct sum
+//#define ≡
+#define ≔    =  // assignment in other langs :=
+#define ⭠    =
+#define ·    & // old style and
+#define ≠    !=
+
+/* Odd symbols I like
+⊚
+⏱
+⮔
+⮈
+⟴
+*/
